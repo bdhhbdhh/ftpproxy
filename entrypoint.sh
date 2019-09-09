2e8f3854576db0a6516f64155e939f98fb035c22
bdhhbdhh <44903340+bdhhbdhh@users.noreply.github.com>
2019-09-09 17:56:03 +0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+param = "-b -D 21"
+
+if [ "$LOCALADDR" ]; then 
+    param = $param + "-q " + $LOCALADDR
+fi
+
+param = $param + " " + $FTPSERVER
+
+/usr/local/sbin/ftp.proxy $param
+wait $!
