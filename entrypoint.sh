97bd322656659e524e0f260caa776b774972b224
bdhhbdhh <44903340+bdhhbdhh@users.noreply.github.com>
2019-09-09 18:13:53 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-param = "-b -D 21"
+param="-b -D 21"
 
 if [ "$LOCALADDR" ]; then 
-    param = $param + "-q " + $LOCALADDR
+    param="$param -q $LOCALADDR"
 fi
 
-param = $param + " " + $FTPSERVER
+param="$param $FTPSERVER"
 
 /usr/local/sbin/ftp.proxy $param
 wait $!
